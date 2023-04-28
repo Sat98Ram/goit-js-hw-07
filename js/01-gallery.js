783b8f73b7b3bfff61071f9bbaf419469c6c10ca
Iryna <irinkasatanovskaya@gmail.com>
2023-04-28 16:20:26 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// 2. Реалізація делегування на ul.gallery і отримання url великого зображення.
+// 3. Підключення скрипту і стилів бібл.отеки модального вікна basicLightbox.
+//    Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// 5. Заміна значення атрибута src елемента < img > в модальному вікні перед відкриттям.
+//    Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map((item) => {
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${item.original}"><img 
+      class="gallery__image"
+      src="${item.preview}" 
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`;
+});
+
+gallery.insertAdjacentHTML("beforeend", galleryItem.join(""));
+
+gallery.addEventListener("click", getBigImage);
+
+function getBigImage(event) {
+  event.preventDefault();
+  if (event.target.tagName !== `IMG`) {
+    return;
+  }
+
+  event.target.classList.add(`modal`);
+
+  const instance = basicLightbox.create(`
+   <img   class="gallery__image"
+   src="${event.target.dataset.source}"
+      >
+`);
+
+  instance.show();
+}
